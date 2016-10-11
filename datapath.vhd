b750bb002855e819e9f651403d1d3b619c4a5993
juli_nana <juli.nana.07@gmail.com>
2016-10-10 22:28:01 -0300

    datapath

--- datapath.vhd
+++ datapath.vhd
@@ -16,72 +16,170 @@ architecture structural of datapath is
     signal InstrF_FD, InstrD_FD : std_logic_vector(31 downto 0);
 
 begin
-
+--Fetch
     fetchA : fetch port map (
-        Jump => ,
-        PcScrM => ,
+        Jump => JumpM,
+        PcScrM => PCSrcM,
         clk => clk,
         reset => reset,
-        PcBranchM => ,
-        InstrF => ,
-        PCF => ,
-        PcPlus4F =>
+        PcBranchM => PCBranchM,
+        InstrF => Instr,
+        PCF => PCPlus4D,
+        PcPlus4F => PCPlus4F
     );
-
+--FF Fe-De
     Instr_FF : flopr port map (
         reset => reset,
         clk => clk,
         d => InstrF,
         q => InstrD
     );
-    PCPlus_FF : flopr port map (reset => reset, clk => clk, d => PCPlus4F, q => PCPlus4D);
 
+    PCPlus4_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => PCPlus4F, 
+        q => PCPlus4D
+    );
+--Decode
     decodeA : decode port map (
-        InstrD => ,
-        ResultW => ,
-        WriteRegW => ,
-        RegWriteW => ,
-        clk => ,
-        Op => ,
-        Funct => ,
-        RD1 => ,
-        RD2 => ,
-        RtD => ,
-        RdD => ,
-        SignImmD => 
+        InstrD => InstrD,
+        ResultW => ResultW,
+        WriteRegW => WriteRegW,
+        RegWriteW => RegWriteW,
+        clk => clk,
+        Op => Op,
+        Funct => Funct,
+        RD1 => RD1,
+        RD2 => RD2,
+        RtD => RtD,
+        RdD => RdD,
+        SignImmD => SignImmD
+    );
+--FF De-Ex
+    RD1_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => RD1, 
+        q => RD2E
     );
 
+    RD2_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => RD2, 
+        q => RD2E
+    );
+
+    Rt_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => RtD, 
+        q => RtE
+    );
+
+    Rd_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => RdD, 
+        q => RdE
+    );
+
+    Sign_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => SignImmD, 
+        q => SignImmE
+    );
+
+    PCPlus41_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => PCPlus4D, 
+        q => PCPlus4E
+    );
+--Execute
     executeA : execute port map (
-        ZeroE => ,
-        RegDst => ,
-        AluSrc => ,
-        RtE => ,
-        RdE => ,
-        AluControl => ,
-        WriteRegE => ,
-        RD1E => ,
-        RD2E => ,
-        PCPlus4E => ,
-        SignImmE => ,
-        AluOutE => ,
-        WriteDataE => ,
-        PCBranchE =>
+        ZeroE => ZeroE,
+        RegDst => RegDstE,
+        AluSrc => ALUSrcE,
+        RtE => RtE,
+        RdE => RdE,
+        AluControl => ALUControlE,
+        WriteRegE => WriteRegE,
+        RD1E => RD1E,
+        RD2E => RD2E,
+        PCPlus4E => PCPlus4E,
+        SignImmE => SignImmE,
+        AluOutE => ALUOutE,
+        WriteDataE => WriteDataE,
+        PCBranchE => PCBranchE
     );
-
+--FF Ex-Mem
+    Zero_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => ZeroE, 
+        q => ZeroM
+    );
+    ALUOut_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => ALUOutE, 
+        q => ALUOutM
+    );
+    WriteData_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => WriteDataE, 
+        q => WriteDataM
+    );
+    WriteReg_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => WriteRegE, 
+        q => WriteRegM
+    );
+    PCBranch_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => PCBranchE, 
+        q => PCBranchM
+    );
+--Memory
     memoryA : memory port map (
-        ReadDataM => ,
-        MemWriteM => ,
-        clk => ,
-        dump => ,
-        AluOutM => ,
-        WriteDataM => 
+        ReadDataM => ReadDataM,
+        MemWriteM => MemWriteM,
+        clk => clk,
+        dump => dump,
+        AluOutM => ALUOutM,
+        WriteDataM => WriteDataM
     );
-
+--FF Mem-Wr
+    ALUOut1_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => ALUOutM, 
+        q => ALUOutW
+    );
+    ReadData_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => ReadDataM, 
+        q => ReadDataW
+    );
+    WriteReg_FF : flopr port map (
+        reset => reset, 
+        clk => clk, 
+        d => WriteRegM, 
+        q => WriteRegW
+    );
+--Writeback
     writebackA : writeback port map (
-        MemToReg => ,
-        ResultW => ,
-        AluOutW => ,
-        ReadDataW => 
+        MemToReg => MemtoRegW,
+        ResultW => ResultW,
+        AluOutW => ALUOutW,
+        ReadDataW => ReadDataW
     );
 
 end architecture;
