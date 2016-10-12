242575b4aa0b6cbb196206a60f5792bd48b76d63
juli_nana <juli.nana.07@gmail.com>
2016-10-12 11:09:13 -0300

    regfile

--- regfile.vhd
+++ regfile.vhd
@@ -10,38 +10,36 @@ port(ra1, ra2, wa3: in std_logic_vector(4 downto 0);
      clk, we3: in std_logic);
 end regfile;
 
-  signal mem : mem_type := (
-    0 => x"abcdabcd",
-    1 => x"12345678",
-    others => x"11111111");
-
-
 architecture arq of regfile is
+
+  --signal mem : mem_type := (
+    --0 => x"abcdabcd",
+    --1 => x"12345678",
+    --others => x"11111111");
 
     type imem_type is array (31 downto 0) of std_logic_vector(31 downto 0);
     signal mem : imem_type := (
     others => x"00000000");
 
-
 begin
 
-process(clk, memory, ra1, ra2, we3, wa3, wd3)
+process(clk, mem, ra1, ra2, we3, wa3, wd3)
 begin
 
   if(clk'event and clk = '1' and we3 = '1') then
-    memory(to_integer(unsigned(wa3))) <=  wd3;
+    mem(to_integer(unsigned(wa3))) <=  wd3;
   end if;
 
   if ra1 = "00000" then
     rd1 <= x"00000000";
   else
-    rd1 <= memory(to_integer(unsigned(ra1)));
+    rd1 <= mem(to_integer(unsigned(ra1)));
   end if;
 
   if ra2 = "00000" then
     rd2 <= x"00000000";
   else
-    rd2 <= memory(to_integer(unsigned(ra2)));
+    rd2 <= mem(to_integer(unsigned(ra2)));
   end if;
 
 end process;
